88ec66ffadc78695f2486204a610e883e445db5e
jinjin0528 <jin12lee@naver.com>
2024-07-22 21:00:45 +0900

    new practice01

---
+++ practice/codingtest/src/main/java/section01/Practice1.java
@@ -0,0 +1,10 @@
+package section01;
+
+public class Practice1 {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+            answer = num1 * num2;
+            return answer;
+        }
+    }
+
